104d92b98b827d44825f8f2b3241e6e381c3b435
Tsukada <seiya.tsukada@accenture.com>
2018-12-20 14:34:33 +0900

    commit

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # coding: utf-8
 
 from jinja2 import Environment, FileSystemLoader
-import path
+import os
 
 def lambda_handler(event, context):
 
-  env = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
-  
-  template = env.get_template('index.html')
-  html = template.render()
-  
-  return {
+    print os.path.join(os.path.dirname(__file__), 'templates')
+
+    env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'), encoding='utf8'))
+
+    template = env.get_template('index.html')
+    html = template.render()
+
+    return {
       "statusCode": 200,
       "headers": {"Content-Type": "text/html"},
       "body": html
-  }
+    }
